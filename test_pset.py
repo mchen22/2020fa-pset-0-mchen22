4935af6e0ad8bb1e24d08b0f47ed287f2e2274a1
MaggieChen <nuoxichen@gmail.com>
2020-09-07 11:37:22 -0400

    Add more test case

--- test_pset.py
+++ test_pset.py
@@ -12,10 +12,13 @@ try:
     # Absent on Windows, trigger AttributeError
     signal.alarm
 
+
     def _timeout(signum, frame):
         raise TimeoutError()
 
+
     signal.signal(signal.SIGALRM, _timeout)
+
 
     @contextmanager
     def timeout(seconds=1, message="Timeout!"):
@@ -68,11 +71,64 @@ class FibTests(TestCase):
 
     def test_summable(self):
         ss = SummableSequence(0, 1)
-        for n in range(0, 50, 5):
+        expected_list = [
+            0,
+            5,
+            55,
+            610,
+            6765,
+            75025,
+            832040,
+            9227465,
+            102334155,
+            1134903170
+        ]
+        for n, expected in zip(range(0, 50, 5), expected_list):
             with timeout(message="Timeout running f({})".format(n)):
-                raise NotImplementedError(
-                    "You should implement this and other SummableSequence tests!"
-                )
+                self.assertEqual(expected, ss(n))
+
+    def test_summable2(self):
+        ss = SummableSequence(5, 7, 11)
+        expected_list = [
+            5,
+            75,
+            9875,
+            27225827,
+            1580050847865,
+            1930226454126341015,
+            49635531989796272182263551,
+            26867318019797218179403409745527301,
+            306128080494170346319739337790574601696291819,
+            73422511876003456020233690509059885219885401186298370231,
+        ]
+        for n, expected in zip(range(0, 50, 5), expected_list):
+            with timeout(message="Timeout running f({})".format(n)):
+                self.assertEqual(expected, ss(n))
+
+    def test_summable3(self):
+        ss = SummableSequence(4, 3, 5, 1)
+        expected_list = [
+            4,
+            22,
+            2097,
+            5515389,
+            386116271219,
+            719286342965742385,
+            35655561324735942072450996,
+            47032113352880057524437041699437173,
+            1650834218405770739677111125570317281318776949,
+            1541892809964900069760927521813608043791336591573838585395
+        ]
+        for n, expected in zip(range(0, 50, 5), expected_list):
+            with timeout(message="Timeout running f({})".format(n)):
+                self.assertEqual(expected, ss(n))
+
+    def test_summable4(self):
+        ss = SummableSequence(0)
+        expected_list = [0]
+        for n, expected in zip(range(5, 50, 5), [expected_list * i for i in range(5, 50, 5)]):
+            with timeout(message="Timeout running f({})".format(n)):
+                self.assertEqual(expected, ss(n))
 
 
 class TestTimeout(TestCase):
